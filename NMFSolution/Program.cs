b99f3b0f7eec05b21969d9f4a357526f08bf4b16
Georg Hinkel <Georg Hinkel>
2017-05-11 20:45:24 +0200

    Add NMF solution

---
+++ NMFSolution/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NMF.Models.Repository;
+using TTC2017.StateElimination.Transitiongraph;
+using System.Diagnostics;
+
+namespace TTC2017.StateElimination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            CalculateRegex(args[0]);
+            stopwatch.Stop();
+            Console.WriteLine($"Operation took {stopwatch.Elapsed.TotalSeconds}s");
+        }
+
+        private static void CalculateRegex(string path)
+        {
+            var repository = new ModelRepository();
+            var transitionGraph = repository.Resolve(path).RootElements[0] as TransitionGraph;
+
+            var initial = transitionGraph.States.FirstOrDefault(s => s.IsInitial);
+            var final = CreateFinal(transitionGraph);
+
+            foreach (var s in transitionGraph.States.ToArray())
+            {
+                if (s == initial || s == final) continue;
+
+                var selfEdge = string.Join("+", from edge in s.Outgoing
+                                                where edge.Target == s
+                                                select edge.Label);
+
+                if (!string.IsNullOrEmpty(selfEdge)) selfEdge = $"({selfEdge})*";
+
+                foreach (var incoming in s.Incoming.Where(t => t.Source != s))
+                {
+                    foreach (var outgoing in s.Outgoing.Where(t => t.Target != s))
+                    {
+                        transitionGraph.Transitions.Add(new Transition
+                        {
+                            Source = incoming.Source,
+                            Target = outgoing.Target,
+                            Label = incoming.Label + selfEdge + outgoing.Label
+                        });
+                    }
+                }
+
+                s.Delete();
+            }
+        }
+
+        private static IState CreateFinal(TransitionGraph transitionGraph)
+        {
+            var finalStates = transitionGraph.States.Where(s => s.IsFinal).ToList();
+            if (finalStates.Count == 1)
+            {
+                return finalStates[0];
+            }
+            else
+            {
+                var newFinal = new State();
+                foreach (var s in finalStates)
+                {
+                    transitionGraph.Transitions.Add(new Transition
+                    {
+                        Source = s,
+                        Target = newFinal
+                    });
+                }
+                transitionGraph.States.Add(newFinal);
+                return newFinal;
+            }
+        }
+    }
+}
---
+++ NMFSolution/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using NMF.Models;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NMFSolution")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NMFSolution")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("62da1408-6f67-4061-b50e-1054d9887cd4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ModelMetadata("http://transitiongraph/1.0", "TTC2017.StateElimination.NMFSolution.TransitionGraph.nmeta")]
